ff1a7b5cb43a5266e37701157a24139da99615ea
Seba Villalobos <sdv@knights.ucf.edu>
2020-08-20 14:15:02 -0400

    formatted pr

--- testing/graphs/Dijkstra_NAQC.cpp
+++ testing/graphs/Dijkstra_NAQC.cpp
@@ -3,57 +3,67 @@ using namespace std;
 
 using ll = long long;
 
-template<class T> pair<vector<bool>, vector<T>> 
-	dijkstra(vector<vector<pair<int, T>>> &adj, int s) {
-	using ST = pair<T, int>; // state type
-	priority_queue<ST, vector<ST>, greater<ST>> q;
-	vector<T> dist(adj.size()); 
-	vector<bool> seen(adj.size()), reached = seen;
-	q.emplace(0, s); seen[s] = 1;
-	while (!q.empty()) {
-		auto [d, u] = q.top(); q.pop();
-		if (reached[u]) continue; 
-		reached[u] = 1;
-		for (auto [v, w] : adj[u])
-			if (!seen[v] || d + w < dist[v])
-				q.emplace(dist[v] = d + w, v), seen[v] = 1;
-	}
-	return {reached, dist};
+template <class T>
+pair<vector<bool>, vector<T>> dijkstra(
+ vector<vector<pair<int, T>>>& adj, int s) {
+  using ST = pair<T, int>; // state type
+  priority_queue<ST, vector<ST>, greater<ST>> q;
+  vector<T> dist(adj.size());
+  vector<bool> seen(adj.size()), reached = seen;
+  q.emplace(0, s);
+  seen[s] = 1;
+  while (!q.empty()) {
+    auto [d, u] = q.top();
+    q.pop();
+    if (reached[u]) continue;
+    reached[u] = 1;
+    for (auto [v, w] : adj[u])
+      if (!seen[v] || d + w < dist[v])
+        q.emplace(dist[v] = d + w, v), seen[v] = 1;
+  }
+  return {reached, dist};
 }
 
 struct Weight {
-	ll d, it;
-	Weight(ll d = 0, ll it = 0): d(d), it(it) { }
-	Weight operator+(const Weight &o) const { return {d + o.d, it + o.it}; }
-	bool operator<(const Weight &o) const { 
-		if (d != o.d) return d < o.d;
-		return it > o.it;
-	}
+  ll d, it;
+  Weight(ll d = 0, ll it = 0) : d(d), it(it) {}
+  Weight operator+(const Weight& o) const {
+    return {d + o.d, it + o.it};
+  }
+  bool operator<(const Weight& o) const {
+    if (d != o.d) return d < o.d;
+    return it > o.it;
+  }
 };
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
 
-	int n; cin >> n;
+  int n;
+  cin >> n;
 
-	vector<int> items(n);
-	vector<vector<pair<int, Weight>>> adj(n);
+  vector<int> items(n);
+  vector<vector<pair<int, Weight>>> adj(n);
 
-	for (int &x : items) cin >> x;
+  for (int& x : items) cin >> x;
 
-	int m; cin >> m;
-	for (int i = 0; i < m; i++) {
-		int u, v, w;
-		cin >> u >> v >> w;
-		u--, v--;
+  int m;
+  cin >> m;
+  for (int i = 0; i < m; i++) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    u--, v--;
 
-		adj[u].emplace_back(v, Weight(w, items[v]));
-		adj[v].emplace_back(u, Weight(w, items[u]));
-	}
+    adj[u].emplace_back(v, Weight(w, items[v]));
+    adj[v].emplace_back(u, Weight(w, items[u]));
+  }
 
-	auto [reached, dist] = dijkstra(adj, 0);
+  auto [reached, dist] = dijkstra(adj, 0);
 
-	if (!reached[n-1]) cout << "impossible\n";
-	else cout << dist[n-1].d << " " << dist[n-1].it + items[0] << "\n";
+  if (!reached[n - 1])
+    cout << "impossible\n";
+  else
+    cout << dist[n - 1].d << " " << dist[n - 1].it + items[0]
+         << "\n";
 }
--- testing/graphs/Dijkstra_kth_popup.cpp
+++ testing/graphs/Dijkstra_kth_popup.cpp
@@ -3,54 +3,58 @@ using namespace std;
 
 using ll = long long;
 
-template<class T> pair<vector<bool>, vector<T>> 
-	dijkstra(vector<vector<pair<int, T>>> &adj, int s) {
-	using ST = pair<T, int>; // state type
-	priority_queue<ST, vector<ST>, greater<ST>> q;
-	vector<T> dist(adj.size()); 
-	vector<bool> seen(adj.size()), reached = seen;
-	q.emplace(0, s); seen[s] = 1;
-	while (!q.empty()) {
-		auto [d, u] = q.top(); q.pop();
-		if (reached[u]) continue; 
-		reached[u] = 1;
-		for (auto [v, w] : adj[u])
-			if (!seen[v] || d + w < dist[v])
-				q.emplace(dist[v] = d + w, v), seen[v] = 1;
-	}
-	return {reached, dist};
+template <class T>
+pair<vector<bool>, vector<T>> dijkstra(
+ vector<vector<pair<int, T>>>& adj, int s) {
+  using ST = pair<T, int>; // state type
+  priority_queue<ST, vector<ST>, greater<ST>> q;
+  vector<T> dist(adj.size());
+  vector<bool> seen(adj.size()), reached = seen;
+  q.emplace(0, s);
+  seen[s] = 1;
+  while (!q.empty()) {
+    auto [d, u] = q.top();
+    q.pop();
+    if (reached[u]) continue;
+    reached[u] = 1;
+    for (auto [v, w] : adj[u])
+      if (!seen[v] || d + w < dist[v])
+        q.emplace(dist[v] = d + w, v), seen[v] = 1;
+  }
+  return {reached, dist};
 }
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
 
-	while (true) {
-		int n, m, q, s;
-		cin >> n >> m >> q >> s;
+  while (true) {
+    int n, m, q, s;
+    cin >> n >> m >> q >> s;
 
-		if ((n | m | q | s) == 0) break;
+    if ((n | m | q | s) == 0) break;
 
-		vector<vector<pair<int, ll>>> adj(n);
+    vector<vector<pair<int, ll>>> adj(n);
 
-		for (int i = 0; i < m; i++) {
-			int u, v, w;
-			cin >> u >> v >> w;
+    for (int i = 0; i < m; i++) {
+      int u, v, w;
+      cin >> u >> v >> w;
 
-			adj[u].emplace_back(v, w);
-		}
+      adj[u].emplace_back(v, w);
+    }
 
-		auto [reached, dist] = dijkstra(adj, s);
+    auto [reached, dist] = dijkstra(adj, s);
 
-		for (int i = 0; i < q; i++) {
-			int u;
-			cin >> u;
+    for (int i = 0; i < q; i++) {
+      int u;
+      cin >> u;
 
-			if (reached[u]) cout << dist[u] << '\n';
-			else cout << "Impossible\n";
-		}
+      if (reached[u])
+        cout << dist[u] << '\n';
+      else
+        cout << "Impossible\n";
+    }
 
-		cout << '\n';
-	}
-
+    cout << '\n';
+  }
 }
