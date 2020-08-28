dc076112122466a72aeae5e9106c81e074388ea9
bquiroga10 <bquiroga10@gmail.com>
2020-08-28 19:29:05 -0400

    Create SCC.h
    Untested though

---
+++ content/graphs/SCC.h
@@ -0,0 +1,34 @@
+struct SCC {
+  int n, cnt, cmps, top;
+  vector<vector<int>> adj;
+  vector<int> pre, scc, low, stk;
+  SCC() {}
+  SCC(const int& n) : n(n), adj(n), stk(n), 
+    pre(n), low(n), scc(n), pre(n, -1) {
+    cnt = cmps = top = 0;
+  }
+  void addEdge(const int &u, const int &v) {
+    adj[u].pb(v);
+  }
+  void run() {
+    for(int i = 0; i < n; i++)
+      if(pre[i] == -1)
+        dfs(i);
+  }
+  void dfs(const int& u) {
+    pre[u] = low[u] = cnt++;
+    stk[top++] = u;
+    for(int v : adj[u]) {
+      if(pre[v] == -1) dfs(v);
+      low[u] = min(low[u], low[v]);
+    }
+    if(low[u] == pre[u]) {
+      while(true) {
+        int v = stk[--top];
+        scc[v] = cmps;  low[v] = n;
+        if(u == v) break;
+      }
+      cmps++;
+    }
+  }
+};
