f41ec3f6aa534576e44a6d9e22a9ec1846dc69f1
lightseba <sdv@knights.ucf.edu>
2020-09-21 21:09:32 -0400

    fixed testing files

--- testing/graphs/Dijkstra_NAQC.cpp
+++ testing/graphs/Dijkstra_NAQC.cpp
@@ -1,28 +1,11 @@
+// Tested on https://open.kattis.com/problems/bigtruck
+
 #include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
 
-template <class T>
-pair<vector<bool>, vector<T>> dijkstra(
- vector<vector<pair<int, T>>>& adj, int s) {
-  using ST = pair<T, int>; // state type
-  priority_queue<ST, vector<ST>, greater<ST>> q;
-  vector<T> dist(adj.size());
-  vector<bool> seen(adj.size()), reached = seen;
-  q.emplace(0, s);
-  seen[s] = 1;
-  while (!q.empty()) {
-    auto [d, u] = q.top();
-    q.pop();
-    if (reached[u]) continue;
-    reached[u] = 1;
-    for (auto [v, w] : adj[u])
-      if (!seen[v] || d + w < dist[v])
-        q.emplace(dist[v] = d + w, v), seen[v] = 1;
-  }
-  return {reached, dist};
-}
+#include "../../content/graphs/Dijkstra.h"
 
 struct Weight {
   ll d, it;
@@ -31,8 +14,7 @@ struct Weight {
     return {d + o.d, it + o.it};
   }
   bool operator<(const Weight& o) const {
-    if (d != o.d) return d < o.d;
-    return it > o.it;
+    return d < o.d || (d == o.d && it > o.it);
   }
 };
 
--- testing/graphs/Dijkstra_kth_popup.cpp
+++ testing/graphs/Dijkstra_kth_popup.cpp
@@ -1,28 +1,11 @@
+// Tested on https://open.kattis.com/problems/shortestpath1
+
 #include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
 
-template <class T>
-pair<vector<bool>, vector<T>> dijkstra(
- vector<vector<pair<int, T>>>& adj, int s) {
-  using ST = pair<T, int>; // state type
-  priority_queue<ST, vector<ST>, greater<ST>> q;
-  vector<T> dist(adj.size());
-  vector<bool> seen(adj.size()), reached = seen;
-  q.emplace(0, s);
-  seen[s] = 1;
-  while (!q.empty()) {
-    auto [d, u] = q.top();
-    q.pop();
-    if (reached[u]) continue;
-    reached[u] = 1;
-    for (auto [v, w] : adj[u])
-      if (!seen[v] || d + w < dist[v])
-        q.emplace(dist[v] = d + w, v), seen[v] = 1;
-  }
-  return {reached, dist};
-}
+#include "../../content/graphs/Dijkstra.h"
 
 int main() {
   cin.tie(0)->sync_with_stdio(0);
