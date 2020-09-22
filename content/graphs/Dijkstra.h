f6d7ce6e9fb26ff7451675eb5308f9127754c686
lightseba <sdv@knights.ucf.edu>
2020-09-21 21:10:27 -0400

    changed source file

--- content/graphs/Dijkstra.h
+++ content/graphs/Dijkstra.h
@@ -1,20 +1,21 @@
 template <class T>
 pair<vector<bool>, vector<T>> dijkstra(
- vector<vector<pair<int, T>>>& adj, int s) {
-  using ST = pair<T, int>; // state type
+ const vector<vector<pair<int, T>>>& adj, int s) {
+  using ST = pair<T, int>; /// state type
   priority_queue<ST, vector<ST>, greater<ST>> q;
   vector<T> dist(adj.size());
-  vector<bool> seen(adj.size()), reached = seen;
-  q.emplace(0, s);
+  vector<bool> seen(adj.size());
+  q.emplace(T{}, s);
   seen[s] = 1;
   while (!q.empty()) {
     auto [d, u] = q.top();
     q.pop();
-    if (reached[u]) continue;
-    reached[u] = 1;
+    if (dist[u] < d) continue;
     for (auto [v, w] : adj[u])
-      if (!seen[v] || d + w < dist[v])
-        q.emplace(dist[v] = d + w, v), seen[v] = 1;
+      if (!seen[v] || d + w < dist[v]) {
+        q.emplace(dist[v] = d + w, v);
+        seen[v] = 1;
+      }
   }
-  return {reached, dist};
+  return {seen, dist};
 }
