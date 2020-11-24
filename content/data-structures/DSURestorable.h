813b5ba08d2e13c9aacfd61259e2cafddc6f5a93
lightseba <sdv@knights.ucf.edu>
2020-11-23 21:53:55 -0500

    stack -> vector

--- content/data-structures/DSURestorable.h
+++ content/data-structures/DSURestorable.h
@@ -7,22 +7,22 @@
 
 struct RestorableDSU {
   vector<int> s;
-  stack<pair<int, int>> stk;
+  vector<pair<int, int>> stk;
   RestorableDSU(int n): s(n, -1) {}
   int find(int i) { return s[i] < 0 ? i : find(s[i]); }
   bool join(int a, int b) {
     a = find(a), b = find(b);
     if (a == b) return false;
     if (s[a] > s[b]) swap(a, b);
-    stk.emplace(a, s[a]);
-    stk.emplace(b, s[b]);
+    stk.emplace_back(a, s[a]);
+    stk.emplace_back(b, s[b]);
     s[a] += s[b], s[b] = a;
     return true;
   }
   int time() { return stk.size(); }
   void revert(int t) {
     while (stk.size() > t)
-      s[stk.top().first] = stk.top().second, stk.pop();
+      s[stk.back().first] = stk.back().second, stk.pop_back();
   }
   int size(int i) { return -s[find(i)]; }
   bool same(int a, int b) { return find(a) == find(b); }
