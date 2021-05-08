adc1c367266838f8d26d0175ad2022852b52c192
lightseba <sdv@knights.ucf.edu>
2021-05-08 17:35:53 -0400

    tabs -> 2 space

--- content/data-structures/IterativeSegmentTree.h
+++ content/data-structures/IterativeSegmentTree.h
@@ -10,21 +10,21 @@
 #pragma once
 
 struct Tree {
-	typedef int T;
-	static constexpr T unit = INT_MIN;
-	T f(T a, T b) { return max(a, b); } // (any associative fn)
-	vector<T> s; int n;
-	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
-	void update(int pos, T val) {
-		for (s[pos += n] = val; pos /= 2;)
-			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
-	}
-	T query(int b, int e) { // query [b, e)
-		T ra = unit, rb = unit;
-		for (b += n, e += n; b < e; b /= 2, e /= 2) {
-			if (b % 2) ra = f(ra, s[b++]);
-			if (e % 2) rb = f(s[--e], rb);
-		}
-		return f(ra, rb);
-	}
+  typedef int T;
+  static constexpr T unit = INT_MIN;
+  T f(T a, T b) { return max(a, b); } // (any associative fn)
+  vector<T> s; int n;
+  Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
+  void update(int pos, T val) {
+    for (s[pos += n] = val; pos /= 2;)
+      s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
+  }
+  T query(int b, int e) { // query [b, e)
+    T ra = unit, rb = unit;
+    for (b += n, e += n; b < e; b /= 2, e /= 2) {
+      if (b % 2) ra = f(ra, s[b++]);
+      if (e % 2) rb = f(s[--e], rb);
+    }
+    return f(ra, rb);
+  }
 };
