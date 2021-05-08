b0546e559c3762d4e5a3e6aca64f2e673625a9b8
lightseba <sdv@knights.ucf.edu>
2021-05-08 17:32:09 -0400

    added hashing stuff

---
+++ content/data-structures/HashMap.h
@@ -0,0 +1,20 @@
+/**
+ * Author: Simon Lindholm, chilli
+ * Date: 2018-07-23
+ * License: CC0
+ * Source: http://codeforces.com/blog/entry/60737
+ * Description: Hash map with mostly the same API as unordered\_map, but \tilde
+ * 3x faster. Uses 1.5x memory.
+ * Initial capacity must be a power of 2 (if provided).
+ */
+#pragma once
+
+#include <bits/extc++.h> /** keep-include */
+// To use most bits rather than just the lowest ones:
+struct chash { // large odd number for C
+	const uint64_t C = ll(4e18 * acos(0)) | 71;
+	ll operator()(ll x) const { return __builtin_bswap64(x*C); }
+};
+__gnu_pbds::gp_hash_table<ll,int,chash> h({},{},{},{},{1<<16});
+template <class T> // for auto resize
+using hash_map = __gnu_pbds::gp_hash_table<ll, T, chash>;
