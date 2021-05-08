d90756fc8a4d02f6d6f322b8a34886fc09111dca
lightseba <sdv@knights.ucf.edu>
2021-05-08 17:13:13 -0400

    made n dim vector shorter

--- content/misc/NDimensionalVector.h
+++ content/misc/NDimensionalVector.h
@@ -5,15 +5,10 @@
  * double b[n][m][k] initialized with 3.14
  */
 
-template <int D, class T>
-struct Vec : public vector<Vec<D - 1, T>> {
-  static_assert(D >= 1,
-                "Vector dimension must be greater than zero!");
-  template <typename... Args>
-  Vec(int n = 0, Args... args):
+template <int D, class T> struct Vec : vector<Vec<D - 1, T>> {
+  template <typename... Args> Vec(int n = 0, Args... args):
     vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...)) {}
 };
-template <typename T>
-struct Vec<1, T> : public vector<T> {
+template <typename T> struct Vec<1, T> : vector<T> {
   Vec(int n = 0, const T& val = T()): vector<T>(n, val) {}
 };
