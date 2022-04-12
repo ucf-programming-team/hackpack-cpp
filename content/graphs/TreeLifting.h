5355c43e5a2bf8d9fcc99de335b2b033416ea21f
Seba Villalobos <svillalobos450@gmail.com>
2022-04-12 14:26:07 -0400

    add one var tree lifting

---
+++ content/graphs/TreeLifting.h
@@ -0,0 +1,39 @@
+/**
+ * Author: lightseba
+ * Date: 2022-04-12
+ * License: CC0
+ * Source: https://codeforces.com/blog/entry/100826
+ * Description: Calculate jumps up a tree,
+ * to support fast upward jumps and LCAs.
+ * Assumes the root node points to itself.
+ * Time: construction $O(N)$, queries $O(\log N)$
+ * Status: Tested
+ */
+#pragma once
+
+struct lift {
+  vi d, p, j;
+  lift(vector<vi>& adj): d(sz(adj)), p(d), j(d) { dfs(0, adj); }
+  void dfs(int u, vector<vi>& adj) {
+    int jmp = (d[u] + d[j[j[u]]] == 2*d[j[u]]) ? j[j[u]] : u;
+    for (int v : adj[u]) if (v != p[u]) {
+      d[v] = d[p[v] = u] + 1, j[v] = jmp;
+      dfs(v, adj);
+    }
+  }
+  int lca(int u, int v) {
+    if (d[u] < d[v]) swap(u, v);
+    while (d[u] > d[v]) u = d[j[u]] >= d[v] ? j[u] : p[u];
+    if (u == v) return u;
+    while (p[u] != p[v]) 
+      if (j[u] != j[v]) u = j[u], v = j[v];
+      else u = p[u], v = p[v];
+    return p[u];
+  }
+  int kth(int u, int k) { 
+    if (k > d[u]) return -1;
+    k = d[u] - k;
+    while (d[u] > k) u = d[j[u]] >= k ? j[u] : p[u];
+    return u;
+  }
+};
---
+++ testing/graphs/TreeLifting.cpp
@@ -0,0 +1,23 @@
+//TEST=https://judge.yosupo.jp/problem/lca
+#include "../content/graphs/TreeLifting.h"
+
+void test() {
+  int n, q;
+  cin >> n >> q;
+
+  vector<vi> adj(n);
+  for (int i = 1; i < n; i++) {
+    int p;
+    cin >> p;
+    adj[p].push_back(i);
+    adj[i].push_back(p);
+  }
+
+  lift t(adj);
+
+  for (int i = 0; i < q; i++) {
+    int u, v;
+    cin >> u >> v;
+    cout << t.lca(u, v) << "\n";
+  }
+}
