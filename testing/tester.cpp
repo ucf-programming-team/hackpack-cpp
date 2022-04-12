14164146f28350d93051d196579f699d28e82476
Seba Villalobos <svillalobos450@gmail.com>
2022-04-12 14:25:35 -0400

    new testing framework

---
+++ testing/tester.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sz(x) int(size(x))
+#define rep(a,b,c) for (int a = (b); a < (c); a++)
+#define all(x) begin(x),end(x)
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+void test();
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    test();
+
+    return 0;
+}
