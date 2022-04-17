0e882d83648983ec87b579e4724a2fa46a308bfc
Seba Villalobos <svillalobos450@gmail.com>
2022-04-17 19:17:18 -0400

    fix formatter

--- doc/scripts/format-all.sh
+++ doc/scripts/format-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 find . -type f | grep -E ".*\.(cpp|h)$" |
-	xargs clang-format-10 -i --style=file
+	xargs clang-format -i --style=file
 
 echo "Formatted:"
 find . -type f | grep -E ".*\.(cpp|h)$"
