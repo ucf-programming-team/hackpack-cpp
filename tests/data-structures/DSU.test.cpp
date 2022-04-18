c8df3a8bd584a64e42f537cc3af2341e6b8f7f0b
Seba Villalobos <svillalobos450@gmail.com>
2022-04-17 22:31:52 -0400

    more ds tests

---
+++ tests/data-structures/DSU.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include "../../tests/TestRunner.h"
+#include "../../content/data-structures/DSU.h"
+
+void test() {
+  int n, q;
+  cin >> n >> q;
+  
+  DSU dsu(n);
+
+  while (q--) {
+    int t, x, y;
+    cin >> t >> x >> y;
+
+    if (t == 0) dsu.join(x, y);
+    else cout << dsu.same(x, y) << "\n";
+  }
+}
---
+++ tests/data-structures/RMQLambda.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+#include "../../tests/TestRunner.h"
+#include "../../content/data-structures/RMQ.h"
+
+void test() {
+  int n, q;
+  cin >> n >> q;
+  
+  vector<int> a(n);
+  for (auto& x : a) cin >> x;
+  // test lambda comparison
+  auto cmp = [&](int x, int y) { return a[x] < a[y]; };
+  vector<int> idx(n);
+  iota(idx.begin(), idx.end(), 0);
+  RMQ<int, decltype(cmp)> rmq(idx, cmp);
+
+  while (q--) {
+    int x, y;
+    cin >> x >> y;
+    cout << a[rmq.query(x, y-1)] << "\n";
+  }
+}
