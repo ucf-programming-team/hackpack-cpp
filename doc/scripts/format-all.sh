2995367975dd77b3f37f1542dbefa7b58b0eccdc
Seba Villalobos <svillalobos450@gmail.com>
2022-04-17 21:43:05 -0400

    update format files

--- doc/scripts/format-all.sh
+++ doc/scripts/format-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 find $1 -type f | grep -E ".*\.(cpp|h)$" |
-	xargs clang-format -i --style=file
+	xargs clang-format -i -style=file
 
 echo "Formatted:"
 find $1 -type f | grep -E ".*\.(cpp|h)$"
