43a32aec15086d624d615a42ba3c9534dddf9adc
Seba Villalobos <svillalobos450@gmail.com>
2022-09-02 16:20:12 -0400

    added BIT kth test

---
+++ tests/data-structures/BITKth.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://atcoder.jp/contests/abc234/tasks/abc234_d"
+#include "../../tests/TestRunner.h"
+#include "../../content/data-structures/BIT.h"
+
+void test() {
+	int n, k;
+	cin >> n >> k;
+
+	BIT<int> bit(n);
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		bit.update(n - x, 1);
+		if (i >= k-1)
+			cout << n - bit.kth(k-1) << "\n"; 
+	}
+}
