dc33b99cf33878ba83bbcc1b8c20a08d10ef8b96
Cameron Custer <custer.cameron@gmail.com>
2022-11-27 20:09:15 -0500

    add dominator tree with tests

---
+++ tests/graphs/dominator_tree.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/dominatortree"
+#include "../../tests/TestRunner.h"
+#include "../../content/graphs/dominator_tree.h"
+
+void test() {
+	int n, m, s;
+	cin >> n >> m >> s;
+
+	vector<vi> adj(n);
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+	}
+
+	auto tree = dominator_tree(adj, s);
+
+	vi p(n, -1);
+	p[s] = s;
+	for (int i = 0; i < n; i++)
+		for (int j : tree[i])
+			p[j] = i;
+
+	for (int i = 0; i < n; i++)
+		cout << p[i] << ' ';
+	cout << '\n';
+}
