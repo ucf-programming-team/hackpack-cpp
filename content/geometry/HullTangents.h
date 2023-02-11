e5e367d61c9f982f51085a81c621238851da6549
lightseba <sdv@knights.ucf.edu>
2023-02-10 19:45:11 -0500

    format hull tangents

--- content/geometry/HullTangents.h
+++ content/geometry/HullTangents.h
@@ -4,21 +4,21 @@
  * Description: Finds the left and right, respectively, tangent 
  * points on convex hull from a point. If the point is colinear 
  * to side(s) of the polygon, the point further away is 
- * returned. Requires ccw and $n \geq 3$.
+ * returned. Requires ccw, $n \geq 3$, and the point be on or 
+ * outside the polygon.
  * Time: O(\log n)
  * Status: tested on ASC 40: A and SER 2016: D
  */
 #pragma once
 #include "Point.h"
 #define cmp(i, j) p.cross(h[i], h[j == n ? 0 : j]) * (R ?: -1)
-template<bool R, class P>
-int getTangent(vector<P> &h, P p) {
-	int n = sz(h), lo = 0, hi = n-1, md;
-	if (cmp(0, 1) >= R && cmp(0, n-1) >= !R) return 0;
+template<bool R, class P> int getTangent(vector<P>& h, P p) {
+	int n = sz(h), lo = 0, hi = n - 1, md;
+	if (cmp(0, 1) >= R && cmp(0, n - 1) >= !R) return 0;
 	while (md = (lo + hi + 1) / 2) {
-		auto a = cmp(md, md+1), b = cmp(md, lo);
-		if (a >= R && cmp(md, md-1) >= !R) return md;
-		if (cmp(lo, lo+1) < R) (a < R && b >= 0 ? lo : hi) = md;
+		auto a = cmp(md, md + 1), b = cmp(md, lo);
+		if (a >= R && cmp(md, md - 1) >= !R) return md;
+		if (cmp(lo, lo + 1) < R) (a < R && b >= 0 ? lo : hi) = md;
 		else (a < R || b <= 0 ? lo : hi) = md;
 	}
 }
