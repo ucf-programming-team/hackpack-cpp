d12f9da16b845b6dbbcf56503b31126b289b5fe4
Daniel West <daniel.lomax.west@gmail.com>
2024-04-08 00:05:32 -0700

    add helpful clarification in manachers

--- content/strings/Manacher.h
+++ content/strings/Manacher.h
@@ -2,7 +2,8 @@
  * Author: User adamant on CodeForces
  * Source: http://codeforces.com/blog/entry/12143
  * Description: For each position in a string, computes p[0][i] = half length of
- *  longest even palindrome around pos i, p[1][i] = longest odd (half rounded down).
+ *  longest even palindrome around pos i (center between i-1 and i),
+ *  p[1][i] = longest odd (half rounded down) (center at i).
  * Time: O(N)
  * Status: Stress-tested
  */
